58c1f07afee12d3f2a500931ff7874d05aa7a15f
mariem.badis <mariem.badis@rhis-solutions.com>
2022-07-28 15:56:51 +0100

    listing cocktails

--- src/app/pages/cocktail-list/cocktail-list.component.ts
+++ src/app/pages/cocktail-list/cocktail-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import axios from 'axios';
 
 @Component({
   selector: 'app-cocktail-list',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CocktailListComponent implements OnInit {
 
-  constructor() { }
+  cocktails = [];
+  nomC;
+
+  constructor(private route: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
+    this.nomC = this.route.snapshot.params.nomC;
+    this.getCocktailByCategorie();
+  }
+
+  getCocktailByCategorie() {
+    axios.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=' + this.nomC)
+      .then(res => {
+        this.cocktails = res.data.drinks;
+        console.log('dada ', this.cocktails);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
 }
